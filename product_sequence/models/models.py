c0f6fad4511a1922e5cb33096738d5f5266d3547
Viltco <58694110+Viltco@users.noreply.github.com>
2021-06-04 11:25:19 +0500

    Add files via upload

---
+++ product_sequence/models/models.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+import re
+
+from odoo import models, fields, api, _
+from odoo.osv import expression
+
+
+class ProductTemplateinh(models.Model):
+    _inherit = 'product.template'
+
+    product_temp_seq = fields.Char('Seq', required=True, copy=False, index=True, readonly=True, default=lambda self: _('New'))
+
+    @api.model
+    def create(self, vals):
+        if vals.get('product_temp_seq', _('New')) == _('New'):
+            vals['product_temp_seq'] = self.env['ir.sequence'].next_by_code('product.template.sequence') or _('New')
+        result = super(ProductTemplateinh, self).create(vals)
+        result.assign_sequence_in_variant()
+        return result
+
+    def write(self, vals):
+        res = super(ProductTemplateinh, self).write(vals)
+        for rec in self:
+            rec.assign_sequence_in_variant()
+        return res
+
+    def assign_sequence_in_variant(self):
+        count = 0
+        for variant in self.product_variant_ids:
+            count += 1
+            variant.product_seq = str(00) + str(00) + str(0) + str(count)
+            variant.item_code = self.product_temp_seq + variant.product_seq
+        return
+
+
+class ProductProductinh(models.Model):
+    _inherit = 'product.product'
+
+    product_seq = fields.Char('Product Code', required=True, copy=False, readonly=True,
+                                index=True, default=lambda self: _('New'))
+    product_counter = fields.Integer()
+    item_code = fields.Char('Item Code')
+
+    def compute_item_code(self):
+        for rec in self:
+            rec.item_code = str(rec.product_counter) #rec.product_tmpl_id.product_temp_seq +
+
+    #     return result
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        if not args:
+            args = []
+        if name:
+            positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
+            product_ids = []
+            if operator in positive_operators:
+                product_ids = list(
+                    self._search(['|', ('default_code', '=', name), ('item_code', 'ilike', name)] + args, limit=limit,
+                                 access_rights_uid=name_get_uid))
+                if not product_ids:
+                    product_ids = list(
+                        self._search(['|', ('barcode', '=', name), ('item_code', 'ilike', name)] + args, limit=limit,
+                                     access_rights_uid=name_get_uid))
+            if not product_ids and operator not in expression.NEGATIVE_TERM_OPERATORS:
+                # Do not merge the 2 next lines into one single search, SQL search performance would be abysmal
+                # on a database with thousands of matching products, due to the huge merge+unique needed for the
+                # OR operator (and given the fact that the 'name' lookup results come from the ir.translation table
+                # Performing a quick memory merge of ids in Python will give much better performance
+                product_ids = list(self._search(args + [('default_code', operator, name)], limit=limit))
+                if not limit or len(product_ids) < limit:
+                    # we may underrun the limit because of dupes in the results, that's fine
+                    limit2 = (limit - len(product_ids)) if limit else False
+                    product2_ids = self._search(args + [('name', operator, name), ('id', 'not in', product_ids)],
+                                                limit=limit2, access_rights_uid=name_get_uid)
+                    product_ids.extend(product2_ids)
+            elif not product_ids and operator in expression.NEGATIVE_TERM_OPERATORS:
+                domain = expression.OR([
+                    ['&', ('default_code', operator, name), ('name', operator, name)],
+                    ['&', ('default_code', '=', False), ('name', operator, name)],
+                ])
+                domain = expression.AND([args, domain])
+                product_ids = list(self._search(domain, limit=limit, access_rights_uid=name_get_uid))
+            if not product_ids and operator in positive_operators:
+                ptrn = re.compile('(\[(.*?)\])')
+                res = ptrn.search(name)
+                if res:
+                    product_ids = list(self._search([('default_code', '=', res.group(2))] + args, limit=limit,
+                                                    access_rights_uid=name_get_uid))
+            # still no results, partner in context: search on supplier info as last hope to find something
+            if not product_ids and self._context.get('partner_id'):
+                suppliers_ids = self.env['product.supplierinfo']._search([
+                    ('name', '=', self._context.get('partner_id')),
+                    '|',
+                    ('product_code', operator, name),
+                    ('product_name', operator, name)], access_rights_uid=name_get_uid)
+                if suppliers_ids:
+                    product_ids = self._search([('product_tmpl_id.seller_ids', 'in', suppliers_ids)], limit=limit,
+                                               access_rights_uid=name_get_uid)
+        else:
+            product_ids = self._search(args, limit=limit, access_rights_uid=name_get_uid)
+        return product_ids
---
+++ tabeer_requisition_sanad/models/models.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from lxml import etree
+
+from odoo.exceptions import UserError
+
+
+class ResUserInh(models.Model):
+    _inherit = 'res.users'
+
+    department_id = fields.Many2one('hr.department')
+
+
+class MaterialRequisitioninh(models.Model):
+    _inherit = 'material.purchase.requisition'
+
+    check_purchases = fields.Boolean("Check Purchase", compute='_compute_state_purchase')
+    # user_id = fields.Many2one('res.users')
+    # check_internal = fields.Boolean("Check Internal", compute='_compute_state_internal')
+    # check_purchase_internal = fields.Boolean("Check Internal Purchase", compute='_compute_state_purchase_internal')
+
+    def _compute_state_purchase(self):
+        for rec in self:
+            purchases = self.env['purchase.order'].search([('origin', '=', rec.name)])
+            internal_pickings = self.env['stock.picking'].search([('origin', '=', rec.name)])
+            p_flag = 0
+            i_flag = 0
+            if purchases:
+                for i in purchases:
+                    if not i.state == 'purchase':
+                        p_flag = 1
+            if internal_pickings:
+                for j in internal_pickings:
+                    if not j.state == 'done':
+                        i_flag = 1
+            if purchases or internal_pickings:
+                if i_flag == 0 and p_flag == 0:
+                    rec.check_purchases = True
+                else:
+                    rec.check_purchases = False
+            else:
+                rec.check_purchases = False
+
+    # def _compute_state_purchase(self):
+    #     for rec in self:
+    #         purchases = self.env['purchase.order'].search([('state', '=', 'purchase'), ('origin', '=', rec.name)])
+    #         if purchases:
+    #             rec.check_purchases = True
+    #         else:
+    #             rec.check_purchases = False
+    #
+    # def _compute_state_internal(self):
+    #     for rec in self:
+    #         internal_pickings = self.env['stock.picking'].search([('state', '=', 'done'), ('origin', '=', rec.name)])
+    #         print(internal_pickings)
+    #         if internal_pickings:
+    #             rec.check_internal = True
+    #         else:
+    #             rec.check_internal = False
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        result = super(MaterialRequisitioninh, self).fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar,
+            submenu=submenu)
+#         if self.env.user.has_group('material_purchase_requisitions.group_purchase_requisition_user') and self.env.user.has_group('tabeer_requisition_sanad.group_requisition_user'):
+#             temp = etree.fromstring(result['arch'])
+#             temp.set('create', '0')
+#             temp.set('edit', '0')
+#             result['arch'] = etree.tostring(temp)
+#         if self.env.user.has_group('material_purchase_requisitions.group_purchase_requisition_user'):
+#             temp = etree.fromstring(result['arch'])
+#             temp.set('create', '0')
+#             temp.set('edit', '0')
+#             result['arch'] = etree.tostring(temp)
+        if not self.env.user.has_group('tabeer_requisition_sanad.group_requisition_user'):
+            temp = etree.fromstring(result['arch'])
+            temp.set('create', '0')
+            temp.set('edit', '0')
+            result['arch'] = etree.tostring(temp)
+        return result
+
+    def action_show_po(self):
+        for rec in self:
+            tree_view_id = self.env.ref('tabeer_requisition_sanad.purchase_order_tree_req').ids
+            form_view_id = self.env.ref('tabeer_requisition_sanad.purchase_order_form_req').ids
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'Purchase',
+                'views': [[tree_view_id, 'tree'], [form_view_id, 'form']],
+                'domain': [('origin', '=', rec.name)],
+                'target': 'current',
+                'res_model': 'purchase.order',
+                'view_mode': 'tree,form',
+            }
+    # def action_received(self):
+    #     for rec in self:
+    #         purchases = self.env['purchase.order'].search([('state', '=', 'purchase')])
+    #         if purchases:
+
+
+    def show_picking(self):
+        for rec in self:
+            tree_view_id = self.env.ref('tabeer_requisition_sanad.vpicktree_req').ids
+            form_view_id = self.env.ref('tabeer_requisition_sanad.view_picking_form_req').ids
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'Purchase',
+                'views': [[tree_view_id, 'tree'], [form_view_id, 'form']],
+                'domain': [('origin', '=', rec.name)],
+                'target': 'current',
+                'res_model': 'stock.picking',
+                'view_mode': 'tree,form',
+            }
+
+    state = fields.Selection([
+        ('draft', 'New'),
+        ('dept_confirm', 'Waiting Department Approval'),
+        ('approve', 'Approved'),
+        ('stock', 'Purchase Order Created'),
+        ('internal', 'Internal Picking Created'),
+        ('stock_internal', 'Purchase & Picking Created'),
+        ('receive', 'Received'),
+        ('cancel', 'Cancelled'),
+        ('reject', 'Rejected')],
+        default='draft',
+        track_visibility='onchange',
+    )
+
+    @api.model
+    def default_get_picking(self):
+        search = self.env['stock.picking.type'].search([('code', '=', 'internal')])
+        return search
+
+    custom_picking_type_id = fields.Many2one(
+        'stock.picking.type',
+        string='Picking Type',
+        copy=False,
+        default=default_get_picking
+
+    )
+
+    def request_stock(self):
+        record = super(MaterialRequisitioninh, self).request_stock()
+        i = 0
+        p = 0
+        for rec in self.requisition_line_ids:
+            if rec.requisition_type == 'internal':
+                i = i + 1
+            if rec.requisition_type == 'purchase':
+                p = p + 1
+
+        if i > 0 and p > 0:
+            self.state = 'stock_internal'
+        elif i > 0 and p == 0:
+            self.state = 'internal'
+        elif i == 0 and p > 0:
+            self.state = 'stock'
+        return record
+
+
+class MaterialRequisition(models.Model):
+    _inherit = 'material.purchase.requisition.line'
+
+    @api.onchange('product_id')
+    def test_action(self):
+        partner_users = self.env['res.users'].search([]).mapped('partner_id')
+        partner_list = []
+        for users in partner_users:
+            obj = self.env['res.users'].search([('partner_id', '=', users.id)])
+            if obj.has_group('purchase.group_purchase_user'):
+                partner_list.append(obj.id)
+        return {'domain': {'partner_id': [('id', 'in', partner_list)]
+                           }}
+
+    @api.onchange('requisition_type', 'product_id', 'uom', 'qty')
+    def hide_add_lines(self):
+        for rec in self.requisition_id:
+            if rec.state not in 'draft':
+                raise UserError('This cannot be done')
+
+
+
+
